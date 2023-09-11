e01dfa356fde72107be946e4aa371c83b70fcb71
javed khan <javed@tradebuilderinc.com>
2023-09-11 18:08:15 +0530

    add withdrowal

--- src/pages/wallet/index.tsx
+++ src/pages/wallet/index.tsx
@@ -65,12 +65,22 @@ export default function About() {
       request_type: "Deposit",
       redirect_url: "https://jionexpay.vercel.app/wallet",
   });
+  const [formData2, setFormData2] = useState({
+    amount: '',
+    cust_name: "Johnatan Smith",
+    cust_phone: "1234567895",
+    cust_address: "Bay Area, San Francisco, CA",
+    request_type: "Withdraw",
+    
+});
  const [errorMessage, setErrorMessage] = useState();
+ const [errorMessage2, setErrorMessage2] = useState();
+ const [message, setMessage] = useState();
   const handleSubmit = async (event:any) => {
     event.preventDefault();
     const headers = {
-      "X-Authorization-Aceess": "d5b338ab2fef5ddf59fb76c9d4db92e0fe5865135a8d4510c092e276e732d26b",
-      "X-Authorization-Public": "3bb6ba17764028ffbe7fa3067ca4cca237b522073164c8217f45c3dcba1453bf",
+      "X-Authorization-Aceess": "905a5dfe7bdb46525841bc924a13525498ef6c0c55be5060c32563c548fbed49",
+      "X-Authorization-Public": "a478c2a1420ca4fd6c86d1e857cd5732c8ef00fb18328734b1ba6e331f1dc413",
       "Content-Type": "application/json",
     };
     try {
@@ -104,11 +114,52 @@ export default function About() {
     const { name, value } = event.target;
     setFormData({ ...formData, [name]: value });
   };
+  const handleWithdrawalSubmit = async (event:any) => {
+    event.preventDefault();
+    const headers = {
+      "X-Authorization-Aceess": "905a5dfe7bdb46525841bc924a13525498ef6c0c55be5060c32563c548fbed49",
+      "X-Authorization-Public": "a478c2a1420ca4fd6c86d1e857cd5732c8ef00fb18328734b1ba6e331f1dc413",
+      "Content-Type": "application/json",
+    };
+    try {
+      console.log(formData?.amount,currentBalance)
+      if(currentBalance.data.balance < formData2?.amount){
+        const string:any = `You can not withdrawal more then current balance `+currentBalance.data.balance;
+        setErrorMessage2(string);
+        
+      }else{
+        const response = await fetch(
+          "https://api.jionexpay.com/api/v1/secure/payment_requests",
+          {
+            method: "POST",
+            headers: headers,
+            body: JSON.stringify(formData2), 
+          } );
+          const data = await response.json();
+      if (data.status == 200) {
+    
+        console.log('Form data submitted successfully');
+        setMessage(data?.msg)
+          
+      } else {
+        setErrorMessage2(data?.msg)
+        console.error('Form submission failed');
+      }
+      }
+    } catch (error) {
+      console.error('An error occurred:', error);
+    }
+  };
+
+  const handleWithdrawalChange = (event:any) => {
+    const { name, value } = event.target;
+    setFormData2({ ...formData2, [name]: value });
+  };
   const [currentBalance, setCurrentBalance]: any = useState([]);
   const getCurrentBalance = async () => {
     const headers = {
-      "X-Authorization-Aceess": "d5b338ab2fef5ddf59fb76c9d4db92e0fe5865135a8d4510c092e276e732d26b",
-      "X-Authorization-Public": "3bb6ba17764028ffbe7fa3067ca4cca237b522073164c8217f45c3dcba1453bf",
+      "X-Authorization-Aceess": "905a5dfe7bdb46525841bc924a13525498ef6c0c55be5060c32563c548fbed49",
+      "X-Authorization-Public": "a478c2a1420ca4fd6c86d1e857cd5732c8ef00fb18328734b1ba6e331f1dc413",
       "Content-Type": "application/json",
     };
     const response = await fetch(
@@ -201,6 +252,32 @@ export default function About() {
                   </div>
                 </div>
               </div>
+              <div className="col-lg-12">
+              <div className="card mb-4">
+                <div className="card-body">
+                  <p className="text-danger">{errorMessage2}</p>
+                  <p className="text-success">{message}</p>
+                  <form onSubmit={handleWithdrawalSubmit}>
+                    <div className="form-group">
+                      <label>Amount</label>
+                      <input
+                        type="number"
+                        name="amount"
+                        className="form-control"
+                        value={formData2.amount}
+                        onChange={handleWithdrawalChange}
+                      />
+                     
+                    </div>
+                    <div className="form-group mt-2 text-end">
+                      <button type="submit" className="btn btn-primary ">
+                      Withdrawal
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
             </div>
             <div className="col-lg-6">
               <div className="card mb-4">
